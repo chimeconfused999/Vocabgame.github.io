91455533013c9e64159aa5f0c87c510352227a34
chimeconfused999 <62970161+chimeconfused999@users.noreply.github.com>
2022-07-11 10:49:57 -0700

    T

--- index.js
+++ index.js
@@ -72,13 +72,21 @@ function SignIn(O) {
     window.addEventListener('load', function() { 
       initApp({}); 
     });
-var myVar;
 
-function myFunction() {
-  myVar = setTimeout(showPage, 3000);
-}
-
-function showPage() {
-  document.getElementById("loader").style.display = "none";
-  document.getElementById("myDiv").style.display = "block";
-}
+const userId = document.getElementById('userId');
+const firstName = document.getElementById('firstName');
+const lastName = document.getElementById('lastName');
+const age = document.getElementById('age');
+const addBtn = document.getElementById('addBtn');
+const updateBtn = document.getElementById('updateBtn');
+const removeBtn = document.getElementById('removeBtn'); 
+const database = firebase.database();
+const usersRef = database.ref('/wordle');
+addBtn.addEventListener('click', e => {
+  e.preventDefault();
+  usersRef.child(userId.value).set({
+    first_name: firstName.value,
+    last_name: lastName.value,
+    age: age.value
+  });
+});
