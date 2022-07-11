f66ff60531ee9fb49780b7b2237f6aa7181320a6
Teapzz <vyom.kumar.elpaso@gmail.com>
2022-07-11 15:03:22 -0700

    Test

--- index.js
+++ index.js
@@ -1,30 +1,53 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyD8SyHAMMVV0Gtcs871LSM0tficArYX5eg",
-  authDomain: "zrenix-aae2e.firebaseapp.com",
-  databaseURL: "https://zrenix-aae2e.firebaseio.com",
-  projectId: "zrenix-aae2e",
-  storageBucket: "zrenix-aae2e.appspot.com",
-  messagingSenderId: "1095365181062",
-  appId: "1:1095365181062:web:db9b34e80b0572fc",
-  measurementId: "G-GR8YWKSF7F"
+function initApp(O) {
+  firebase.auth().onAuthStateChanged(function(user) {
+   if (user) { 
+     uinfo = user; email=user.email; 
+     LoggedOutDisplay({id:'Login',photo:0},user);
+   } else {  LoginDisplay({id:'Login'});  }
+  }, function(error) { console.log(error); }
+  );
 };
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+function test(O) {
+  db.doc('/public/v').onSnapshot(function(doc) {
+    console.log(doc.data());
+  });
+}
 
-const GoogleAuth = new firebase.auth.GoogleAuthProvider();
+function SignIn(O) {
+    var errors;
+    if(O.provider=='microsoft') {  var provider = new firebase.auth.OAuthProvider('microsoft.com');
+    } else {  var provider = new firebase.auth.GoogleAuthProvider(); 
+    }
+    if(O.option=='Redirect') { // Sign-in with redirect
+      firebase.auth().signInWithRedirect(provider);
+      firebase.auth().getRedirectResult().then(function(result) {
+          if (result.credential) { var token = result.credential.accessToken; }
+          var user = result.user;
+      });
+    } else { // Sign-in with Popup [Default]
+      firebase.auth().signInWithPopup(provider).then(function(result) {
+          var token = result.credential.accessToken, user = result.user;
+      }).catch(function(error) { errors = error; });
+    }
+  }
 
-//To sign in with pop-up.
-firebase.auth().signInWithPopup(googleAuth);
+  function SignOut(O) { firebase.auth().signOut().then(function(results) {}).catch(function(error) { errors = error;}); }
 
-//To sign in with redirect. 
-firebase.auth().signInWithRedirect(googleAuth);
+    function LoginDisplay(O) { 
+      var id= O.hasOwnProperty('id')? O.id : 'Login';
+     $('#'+id).html(
+       '<span>Sign-in with </span> ' 
+       + '<button onclick="SignIn({option:'+"'Redirect'"+'});">Google</button> '
+       + '<button onclick="SignIn({option:' + "'Redirect',provider:'microsoft'" + '});">Microsoft</button>'
+     );
+    } 
+    function LoggedOutDisplay(O,user) { 
+      var id= O.hasOwnProperty('id')? O.id : 'Login';
+      var n = '<span title='+user.email+'>'+user.displayName+'</span>'; 
+      var signout = `${n} <button onclick="SignOut({});">Logout</button> `; 
+      $('#'+id).html( signout);
+    } 
+
+
+    
