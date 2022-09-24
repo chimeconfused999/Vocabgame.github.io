b6d4c5cc7d1fd21d13c85299fe42fb3cd3b9efec
Teapz <vyom.kumar.elpaso@gmail.com>
2022-09-24 14:43:52 -0700

    Server-side login

--- index.js
+++ index.js
@@ -1,18 +1,13 @@
 function initApp(O) {
   firebase.auth().onAuthStateChanged(function(user) {
    if (user) { 
-     uinfo = user; email=user.email; 
+     uinfo = user; email=user.email;
      LoggedOutDisplay({id:'Login',photo:0},user);
    } else {  LoginDisplay({id:'Login'});  }
   }, function(error) { console.log(error); }
   );
 };
 
-function test(O) {
-  db.doc('/public/v').onSnapshot(function(doc) {
-    console.log(doc.data());
-  });
-}
 
 function SignIn(O) {
     var errors;
@@ -22,9 +17,11 @@ function SignIn(O) {
     if(O.option=='Redirect') { // Sign-in with redirect
       firebase.auth().signInWithRedirect(provider);
       firebase.auth().getRedirectResult().then(function(result) {
-          if (result.credential) { var token = result.credential.accessToken; }
-          var user = result.user;
-          document.getElementById("Middle1").style.display="initial"
+          if (result.credential) { 
+            var token = result.credential.accessToken; 
+            console.log(user + "logged in!")
+            document.getElementById("Middle1").style.display = "none";
+          }
       });
     } else { // Sign-in with Popup [Default]
       firebase.auth().signInWithPopup(provider).then(function(result) {
@@ -45,35 +42,7 @@ function SignIn(O) {
     } 
     function LoggedOutDisplay(O,user) { 
       var id= O.hasOwnProperty('id')? O.id : 'Login';
-      var n = '<span title='+user.email+'>'+user.displayName+'>'+user.photo+'</span>';  
+      var n = '<span title='+user.email+'>'+user.displayName+'</span>'; 
       var signout = `${n} <button onclick="SignOut({});">Logout</button> `; 
       $('#'+id).html( signout);
     } 
-    var name, email, emailVerified, uid, uinfo={};
-    var firebaseConfig = {
-      apiKey: "AIzaSyD8SyHAMMVV0Gtcs871LSM0tficArYX5eg",
-      authDomain: "zrenix-aae2e.firebaseapp.com",
-      databaseURL: "https://zrenix-aae2e.firebaseio.com",
-      projectId: "zrenix-aae2e",
-      storageBucket: "zrenix-aae2e.appspot.com",
-      messagingSenderId: "1095365181062",
-      appId: "1:1095365181062:web:db9b34e80b0572fc",
-      measurementId: "G-GR8YWKSF7F"
-    };
-    firebase.initializeApp(firebaseConfig); 
-    var  editors=[], qdata={}, nchoice=0, realtime, debug=0, role='student', admin=0, db = firebase.firestore();
-
-    
-    window.addEventListener('load', function() { 
-      initApp({}); 
-    });
-var myVar;
-
-function myFunction() {
-  myVar = setTimeout(showPage, 3000);
-}
-
-function showPage() {
-  document.getElementById("loader").style.display = "none";
-  document.getElementById("myDiv").style.display = "block";
-}
